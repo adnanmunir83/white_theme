c725b2aa753d8043800c4acb22ea66ad56375685
adnanmunir83 <adnan_munir@live.com>
2019-12-04 23:12:52 +0400

    Update hooks.py

--- white_theme/hooks.py
+++ white_theme/hooks.py
@@ -16,7 +16,7 @@ app_license = "MIT"
 
 # include js, css files in header of desk.html
 # app_include_css = "/assets/white_theme/css/white_theme.css"
- app_include_js = "/assets/white_theme/js/help_menu.js"
+app_include_js = "/assets/white_theme/js/help_menu.js"
 
 # include js, css files in header of web template
 # web_include_css = "/assets/white_theme/css/white_theme.css"
